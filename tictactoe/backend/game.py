026877b34aa24117cccbad307d9f3b24e241c4a3
Tomás Couso Coddou <tcouso@pop-os.localdomain>
2021-12-31 16:44:04 -0300

    Fix: Methods without self argument fixed

--- tictactoe/backend/game.py
+++ tictactoe/backend/game.py
@@ -7,31 +7,31 @@ class Game:
     def __init__(self) -> None:
         pass
 
-    def horizontal_win(state):
+    def horizontal_win(self, state):
         for i in range(len(state)):
             if state[i][0] == state[i][1] == state[i][2]:
                 return True
         return False
 
-    def vertical_win(state):
+    def vertical_win(self, state):
         for i in range(len(state)):
             if state[0][i] == state[1][i] == state[2][i]:
                 return True
         return False
 
-    def diagonal1_win(state):
+    def diagonal1_win(self, state):
         "Northwest to southeast win."
         if state[0][0] == state[1][1] == state[2][2]:
             return True
         return False
 
-    def diagonal2_win(state):
+    def diagonal2_win(self, state):
         "Southwest to northeast win."
         if state[2][0] == state[1][1] == state[0][2]:
             return True
         return False
 
-    def full_grid(state):
+    def full_grid(self, state):
         for row in state:
             for slot in row:
                 if slot == "":
@@ -51,7 +51,7 @@ class Game:
         else:
             return False
 
-    def to_move(state):
+    def to_move(self, state):
         """Asumes that max plays x's and always goes first"""
         x_count = 0
         o_count = 0
@@ -87,7 +87,7 @@ class Game:
             # no win
             return 0
 
-    def actions(state):
+    def actions(self, state):
         """Returns the set of legal moves available from a grid state."""
         available_moves = set()
         for row_index, row in enumerate(state):
