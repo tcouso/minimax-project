786c66332cee728597412d780b127fdc5192d13a
Tomás Couso Coddou <tcouso@pop-os.localdomain>
2022-01-11 11:30:50 -0300

    Feat: game thread module added

--- tictactoe/backend/minimax_search.py
+++ tictactoe/backend/minimax_search.py
@@ -33,36 +33,3 @@ def min_value(game, state):
         if value_2 < value:
             value, move = (value_2, action)
     return (value, move)
-
-
-def print_board(board):
-    for row in board:
-        render_string = list(map(lambda x: " " if x == "" else x, row))
-        print(render_string)
-
-
-def game_thread_function():
-    game = Game()
-    board = [
-        ["", "", ""],
-        ["", "", ""],
-        ["", "", ""]
-        ]
-    
-    while not game.is_terminal(state=board):
-        state = deepcopy(board)
-        if game.to_move(board) == "x":
-            print("max moves")
-            max_move = minimax_search(game=game, state=state)
-            board = game.result(state=board, action=max_move)
-            print_board(board)
-        elif game.to_move(board) == "o":
-            print("min moves")
-            # here we need to recover a move from socket
-            min_move = eval(input("Ingrese movimiento: "))
-            board = game.result(state=board, action=min_move)
-            print_board(board)
-
-
-if __name__ == "__main__":
-    game_thread_function()
