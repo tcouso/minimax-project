2f1dc08e7e1b121cb1dd02c729ea3b0e2cba5c94
Tomás Couso Coddou <tcouso@pop-os.localdomain>
2022-05-02 22:03:24 -0400

    Fix: slots components refactor

--- minimax-app/src/components/Board.js
+++ minimax-app/src/components/Board.js
@@ -1,10 +1,8 @@
 import React, { Fragment, useState } from "react";
-import "./Board.css";
+import "./Styles.css";
 
 // components
 import Square from './Square';
-
-
 
 
 const Board = () => {
@@ -19,54 +17,18 @@ const Board = () => {
     }
     return (
         <Fragment>
-            <h1>Input Board</h1>
             <div className="board">
-                    <Square
-                        id={0}
-                        slotState={slots[0]}
-                        onClick={handleClick}
-                    />
-                    <Square
-                        id={1}
-                        slotState={slots[1]}
-                        onClick={handleClick}
-                    />
-                    <Square
-                        id={2}
-                        slotState={slots[2]}
-                        onClick={handleClick}
-                    />
-                    <Square
-                        id={3}
-                        slotState={slots[3]}
-                        onClick={handleClick}
-                    />
-                    <Square
-                        id={4}
-                        slotState={slots[4]}
-                        onClick={handleClick}
-                    />
-                    <Square
-                        id={5}
-                        slotState={slots[5]}
-                        onClick={handleClick}
-                    />
-                    <Square
-                        id={6}
-                        slotState={slots[6]}
-                        onClick={handleClick}
-                    />
-                    <Square
-                        id={7}
-                        slotState={slots[7]}
-                        onClick={handleClick}
-                    />
-                    <Square
-                        id={8}
-                        slotState={slots[8]}
-                        onClick={handleClick}
-                    />
-                
+                {slots.map((item, index) => {
+                    return (
+                        <Fragment>
+                            <Square
+                            id={index}
+                            slotState={item}
+                            onClick={handleClick}
+                            />
+                        </Fragment>
+                    )
+                })}
             </div>
         </Fragment>
     );
--- minimax-app/src/components/Square.js
+++ minimax-app/src/components/Square.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Square.css";
+import "./Styles.css";
 
 
 
