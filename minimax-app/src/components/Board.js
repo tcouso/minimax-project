3c8b9625e2471d986b9967fe365cc2b88fc9be5a
Tomás Couso Coddou <tcouso@pop-os.localdomain>
2022-05-04 00:57:55 -0400

    Feat: Conection to api implemented

--- minimax-app/src/components/Board.js
+++ minimax-app/src/components/Board.js
@@ -4,34 +4,48 @@ import "./Styles.css";
 // components
 import Square from './Square';
 
+// urls
+const BASE = "http://127.0.0.1:5000/move";
 
 const Board = () => {
-    
-    const [slots, setSlots] = useState(["","","","","","","","",""]);
-    
-    const handleClick = (event) => {
-        const indexToChange = event.target.id
-        let newSlots = [...slots];
-        newSlots[indexToChange] = "X";
-        setSlots(newSlots);
-    }
-    return (
-        <Fragment>
-            <div className="board">
-                {slots.map((item, index) => {
-                    return (
-                        <Fragment>
-                            <Square
-                            id={index}
-                            slotState={item}
-                            onClick={handleClick}
-                            />
-                        </Fragment>
-                    )
-                })}
-            </div>
-        </Fragment>
-    );
+
+	const [slots, setSlots] = useState(["", "", "", "", "", "", "", "", ""]);
+
+	const handleClick = async (event) => {
+		try {
+			const indexToChange = event.target.id;
+			let newSlots = [...slots];
+			newSlots[indexToChange] = "x";
+			const body = {"board": newSlots};
+			const response = await fetch(BASE, {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(body)
+			});
+			const new_board = await response.json()
+			setSlots(new_board["new-board"]);
+		} catch (err) {
+			console.error(err.message);
+		}
+	};
+		
+	return (
+		<Fragment>
+			<div className="board">
+				{slots.map((item, index) => {
+					return (
+						<Fragment>
+							<Square
+								id={index}
+								slotState={item}
+								onClick={handleClick}
+							/>
+						</Fragment>
+					)
+				})}
+			</div>
+		</Fragment>
+	);
 };
 
 export default Board;
