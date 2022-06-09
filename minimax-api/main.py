41bfcf5bdee25e24bd00da7249b7d557778f59a0
Tomás Couso Coddou <tcouso@pop-os.localdomain>
2022-06-09 00:38:41 -0400

    Fix: Ignore unnecesary files

--- minimax-api/main.py
+++ minimax-api/main.py
@@ -1,4 +1,3 @@
-import re
 from flask import Flask, json, request
 from flask_cors import CORS
 from alpha_beta_search import alpha_beta_search
@@ -13,11 +12,9 @@ game = Game()
 def compute_move():
     raw_board = json.loads(request.data)["board"]
     board = [raw_board[0:3], raw_board[3:6], raw_board[6:9]]
-    print(board)
     agent_move = alpha_beta_search(game=game, state=board)
     new_board_raw = game.result(state=board, action=agent_move)
     new_board = new_board_raw[0] + new_board_raw[1] + new_board_raw[2]
-    # print(new_board)
     response = json.dumps({"new-board": new_board})
     return response
 
